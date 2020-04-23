1b5f8ff426c017f7d9eb77cce67285de0eaf27c7
NoSameRain <1419911360@qq.com>
2020-04-24 00:36:58 +0800

    demo

--- src/App.js
+++ src/App.js
@@ -5,30 +5,23 @@ import PrivateRoute from './components/privateRoute/privateRoute'
 import {Login} from './components/login/login'
 import {Container} from './components/container/container'
 import {Error404} from './components/error/404'
-import {rank} from './components/rank/rank'
-import {ActivityPanel} from "./components/activityPanel/activityPanel";
-import {ArticlePanel} from "./components/articlePanel/articlePanel";
-import {ClubPanel} from "./components/clubPanel/clubPanel";
-
+import "./iconfont.css" //0421
 class App extends React.Component {
-    render() {
-        return (
-            <Router>
-              <Switch>
-                <Route path={'/'} exact={true} render={() => {
-                    return <Redirect to={'/admin/index'}/> //重定向初识界面
-                }}/>
-                <Route path={'/admin/login'} exact={true} component={Login}/>
-                  <Route path={'/admin/rank'} exact={true} component={rank}/>
-
-                  <PrivateRoute path={'/admin'} component={Container}/>
-                <Route path={'/error/404'} exact={true} component={Error404}/>
-                <Redirect to={'/error/404'}/>
-
-              </Switch>
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path={'/'} exact={true} render={() => {
+            return <Redirect to={'/admin/index'}/>
+          }}/>
+          <Route path={'/admin/login'} exact={true} component={Login}/>
+          <PrivateRoute path={'/admin'} component={Container}/>
+          <Route path={'/error/404'} exact={true} component={Error404}/>
+          <Redirect to={'/error/404'}/>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App
--- src/components/container/container.js
+++ src/components/container/container.js
@@ -12,55 +12,49 @@ import {ArticlePanel} from '../articlePanel/articlePanel'
 const {Header, Content, Footer} = Layout
 
 export class Container extends React.Component {
-    gotoIndex() {
-        this.props.history.push('/admin/index')
-    }
+  gotoIndex() {
+    this.props.history.push('/admin/index')
+  }
 
-    gotoRank() {
-        this.props.history.push('/admin/rank')
-    }
+  logout() {
+    $delete('/session/logout').then(res => {
+      this.props.history.push('/admin/login')
+    })
+  }
 
-    logout() {
-        $delete('/session/logout').then(res => {
-            this.props.history.push('/admin/login')
-        })
-    }
-
-    render() {
-        let {match} = this.props
-        return (
-            <Layout className="layout">
-                <Header>
-                    <div className="logo"/>
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['1']}
-                        style={{lineHeight: '64px'}}
-                    >
-                        <Menu.Item onClick={this.gotoIndex.bind(this)} key="1">主页</Menu.Item>
-                        <Menu.Item onClick={this.logout.bind(this)} key="2">登出</Menu.Item>
-                        <Menu.Item onClick={this.gotoRank.bind(this)} key="3">活动评价</Menu.Item>
-                    </Menu>
-                </Header>
-                <Content style={{padding: '0 50px'}}>
-                    <Breadcrumb style={{margin: '16px 0'}}>
-                        <Breadcrumb.Item>社团管理</Breadcrumb.Item>
-                        {/*<Breadcrumb.Item>List</Breadcrumb.Item>*/}
-                        {/*<Breadcrumb.Item>App</Breadcrumb.Item>*/}
-                    </Breadcrumb>
-                    <div style={{background: '#fff', padding: 24}}>
-                        <Switch>
-                            <PrivateRoute path={match.url + '/index'} exact component={ClubPanel}/>
-                            <PrivateRoute path={match.url + '/:club_id/activity/index'} exact component={ActivityPanel}/>
-                            <PrivateRoute path={match.url + '/:club_id/article/index'} exact component={ArticlePanel}/>
-
-                            <Redirect to={'/error/404'}/>
-                        </Switch>
-                    </div>
-                </Content>
-                <Footer style={{textAlign: 'center'}}>BuaaClubs ©2019 Created by BuaaRedSun</Footer>
-            </Layout>
-        )
-    }
+  render() {
+    let {match} = this.props
+    return (
+      <Layout className="layout">
+        <Header>
+          <div className="logo"/>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{lineHeight: '64px'}}
+          >
+            <Menu.Item onClick={this.gotoIndex.bind(this)} key="1">主页</Menu.Item>
+            <Menu.Item onClick={this.logout.bind(this)} key="2">登出</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{padding: '0 50px'}}>
+          <Breadcrumb style={{margin: '16px 0'}}>
+            <Breadcrumb.Item>社团管理</Breadcrumb.Item>
+            {/*<Breadcrumb.Item>List</Breadcrumb.Item>*/}
+            {/*<Breadcrumb.Item>App</Breadcrumb.Item>*/}
+          </Breadcrumb>
+          <div style={{background: '#fff', padding: 24}}>
+            <Switch>
+              <PrivateRoute path={match.url + '/index'} exact component={ClubPanel}/>
+              <PrivateRoute path={match.url + '/:club_id/activity/index'} exact component={ActivityPanel}/>
+              <PrivateRoute path={match.url + '/:club_id/article/index'} exact component={ArticlePanel}/>
+              <Redirect to={'/error/404'}/>
+            </Switch>
+          </div>
+        </Content>
+        <Footer style={{textAlign: 'center'}}>BuaaClubs ©2019 Created by BuaaRedSun</Footer>
+      </Layout>
+    )
+  }
 }
