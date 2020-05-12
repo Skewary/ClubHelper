8938e16646241d99c73afa3870548925091bd2b8
ame <ame-lm@qq.com>
2020-05-12 08:13:13 +0800

    beta integrated vertion

--- src/components/error/404.js
+++ src/components/error/404.js
@@ -4,17 +4,17 @@ import {Button, Card} from 'antd'
 import './404.css'
 
 export class Error404 extends React.Component {
-    gotoIndex = () => {
-        this.props.history.push('/admin/index')
-    }
+  gotoIndex = () => {
+    this.props.history.push('/admin/index')
+  }
 
-    render() {
-        return <div className={'not-found-area'}>
-            <Card className={'not-found-card'} headStyle={{textAlign: 'center'}} bordered={false}
-                  style={{width: 500}} cover={<img alt={'404'}
-                                                   src={'https://club-app-public-1259249016.cos.ap-beijing.myqcloud.com/images/global/404.jpeg'}/>}>
-            </Card>
-            <Button type={'primary'} onClick={this.gotoIndex}>点击跳转到主页</Button>
-        </div>
-    }
+  render() {
+    return <div className={'not-found-area'}>
+      <Card className={'not-found-card'} headStyle={{textAlign: 'center'}} bordered={false}
+            style={{width: 500}} cover={<img alt={'404'}
+                                             src={'https://club-app-public-1259249016.cos.ap-beijing.myqcloud.com/images/global/404.jpeg'}/>}>
+      </Card>
+      <Button type={'primary'} onClick={this.gotoIndex}>点击跳转到主页</Button>
+    </div>
+  }
 }
---
+++ src/components/pictureUploader/pictureUploader.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import {Upload, message, Button, Icon} from 'antd'
+
+export class PictureUploader extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fileList: [],
+      max: this.props.max
+    }
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    if (!nextProps.fileList) {
+      this.setState({
+        fileList: []
+      })
+    }
+  }
+
+  handleChange = info => {
+    let fileList = [...info.fileList]
+    const {max} = this.state
+    fileList = fileList.slice(-max)
+    this.setState({
+      fileList: fileList
+    })
+    const onChange = this.props.onChange
+    if (onChange) {
+      onChange(fileList)
+    }
+  }
+
+  beforeUpload = file => {
+    const tooLarge = file.size > 1 * 1024 * 1024
+    if (tooLarge) {
+      message.error('图片大小必需在1MB以内，否则将无法提交成功')
+    }
+    return !tooLarge
+  }
+
+  render() {
+    const props = {
+      name: 'image',
+      action: '/api/image',
+      onChange: this.handleChange,
+      withCredentials: true,
+      listType: 'picture',
+      accept: '.jpg,.jpeg,.png,.svg',
+      beforeUpload: this.beforeUpload
+    }
+    return (
+      <Upload {...props} fileList={this.state.fileList}>
+        <Button>
+          <Icon type={'upload'}/>点此上传图片
+        </Button>
+      </Upload>
+    )
+  }
+
+}
